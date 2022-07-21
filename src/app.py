02057f58d7666137bee2b6775dd023a58609392f
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-07-21 19:46:37 +0300

    Change error handlers

--- src/app.py
+++ src/app.py
@@ -23,15 +23,27 @@ def create_app():
 
     @application.exception_handler(IntegrityError)
     async def handler_alchemy_integrity_error(request: Request, integrity_err):
-        message = integrity_err.args[0] or str(integrity_err)
+        err_name = "sqlalchemy.exc.IntegrityError"
+        traceback = integrity_err.args[0] or str(integrity_err)
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                            content={"message": message})
+                            content={"message": {'err_name': err_name,
+                                                 'traceback': traceback}})
 
     @application.exception_handler(ProgrammingError)
     async def handler_alchemy_integrity_error(request: Request, programming_err):
-        message = programming_err.args[0] or str(programming_err)
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                            content={"message": message})
+        err_name = "sqlalchemy.exc.ProgrammingError"
+        traceback = programming_err.args[0] or str(programming_err)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={"message": {'err_name': err_name,
+                                                 'traceback': traceback}})
+
+    @application.exception_handler(AttributeError)
+    async def handler_alchemy_integrity_error(request: Request, attribute_err):
+        err_name = "AttributeError"
+        traceback = attribute_err.args or str(attribute_err)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={"message": {'err_name': err_name,
+                                                 'traceback': traceback}})
 
     return application
 
