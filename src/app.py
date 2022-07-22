628766ad011bf5fec090daff6c807697eaf3de83
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-07-22 09:53:44 +0300

    Add logger into handlers

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from sqlalchemy.exc import IntegrityError, ProgrammingError
 
 from .routers import client, order, schedule, table
 from .utils.exceptions import JSONException
+from .logger.main import logger
 
 
 def create_app():
@@ -18,11 +19,13 @@ def create_app():
     # Exception handlers
     @application.exception_handler(JSONException)
     async def error_handler_400(request: Request, exception: JSONException):
+        logger.exception(exception)
         return JSONResponse(status_code=exception.status_code,
                             content={"message": exception.message})
 
     @application.exception_handler(IntegrityError)
     async def handler_alchemy_integrity_error(request: Request, integrity_err):
+        logger.exception(integrity_err)
         err_name = "sqlalchemy.exc.IntegrityError"
         traceback = integrity_err.args[0] or str(integrity_err)
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
@@ -31,6 +34,7 @@ def create_app():
 
     @application.exception_handler(ProgrammingError)
     async def handler_alchemy_integrity_error(request: Request, programming_err):
+        logger.exception(programming_err)
         err_name = "sqlalchemy.exc.ProgrammingError"
         traceback = programming_err.args[0] or str(programming_err)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -39,6 +43,7 @@ def create_app():
 
     @application.exception_handler(AttributeError)
     async def handler_alchemy_integrity_error(request: Request, attribute_err):
+        logger.exception(attribute_err)
         err_name = "AttributeError"
         traceback = attribute_err.args or str(attribute_err)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
