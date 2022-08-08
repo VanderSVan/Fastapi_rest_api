4c500960c2f0025a7ad653591464edbaffd9be65
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-08-08 12:51:20 +0300

    Add swagger for user routers

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from dataclasses import asdict
 
 from fastapi import Depends, Query, Path, status
 from fastapi.responses import JSONResponse
@@ -7,13 +7,18 @@ from fastapi_utils.inferring_router import InferringRouter
 from sqlalchemy.orm import Session
 
 from src.models.user import UserModel
-from src.schemes.user.base_schemes import (UserGetSchema,
-                                           UserPatchSchema,
-                                           UserPostSchema)
-from src.schemes.user.response_schemes import (UserResponsePatchSchema,
-                                               UserResponseDeleteSchema,
-                                               UserResponsePostSchema)
 from src.crud_operations.user import UserOperation
+from src.swagger.user import (
+    UserInterfaceGetAll,
+    UserInterfacePatch,
+    UserInterfacePost,
+
+    UserOutputGetAll,
+    UserOutputGet,
+    UserOutputDelete,
+    UserOutputPatch,
+    UserOutputPost
+)
 from src.utils.dependencies.db import get_db
 from src.utils.dependencies.auth import get_current_admin_or_superuser
 from src.utils.responses.main import get_text
@@ -31,22 +36,17 @@ class User:
     def __init__(self):
         self.user_operation = UserOperation(db=self.db)
 
-    ###############################################################
-    @router.get("/users/", response_model=list[UserGetSchema], status_code=status.HTTP_200_OK)
+    @router.get("/users/", **asdict(UserOutputGetAll()))
     def get_all_users(self,
-                      phone: str = Query(default=None, description="Phone number"),
-                      status: Literal['confirmed'] | Literal['unconfirmed'] = Query(
-                          default=None, description="'confirmed' or 'unconfirmed'"
-                      )
+                      user: UserInterfaceGetAll = Depends(UserInterfaceGetAll)
                       ) -> list[UserModel] | list[None]:
         """
         Returns all users from db by parameters.
         Only available to admins.
         """
-        return self.user_operation.find_all_by_params(phone=phone, status=status)
+        return self.user_operation.find_all_by_params(phone=user.phone, status=user.status)
 
-    ###############################################################
-    @router.get("/users/{user_id}", response_model=UserGetSchema, status_code=status.HTTP_200_OK)
+    @router.get("/users/{user_id}", **asdict(UserOutputGet()))
     def get_user(self, user_id: int = Path(..., ge=1)) -> UserModel | None:
         """
         Returns one user from db by user id.
@@ -54,47 +54,46 @@ class User:
         """
         return self.user_operation.find_by_id(user_id)
 
-    ###############################################################
-    @router.patch("/users/{user_id}", response_model=UserResponsePatchSchema, status_code=status.HTTP_200_OK)
-    def patch_user(self,
-                   user: UserPatchSchema,
-                   user_id: int = Path(..., ge=1)
-                   ) -> JSONResponse:
-        """
-        Updates user data.
-        Only available to admins.
-        """
-        self.user_operation.update_obj(user_id, user)
-
-        return JSONResponse(status_code=status.HTTP_200_OK,
-                            content={"message": get_text('patch').format(
-                                self.user_operation.response_elem_name, user_id)}
-                            )
-
-    ###############################################################
-    @router.delete("/users/{user_id}", response_model=UserResponseDeleteSchema, status_code=status.HTTP_200_OK)
-    def delete_user(self, user_id: int) -> JSONResponse:
+    @router.delete("/users/{user_id}", **asdict(UserOutputDelete()))
+    def delete_user(self, user_id: int = Path(..., ge=1)) -> JSONResponse:
         """
          Deletes user from db by user id.
          Only available to admins.
          """
         self.user_operation.delete_obj(user_id)
 
-        return JSONResponse(status_code=status.HTTP_200_OK,
-                            content={"message": get_text('delete').format(
-                                self.user_operation.response_elem_name, user_id)}
-                            )
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"message": get_text('delete').format(
+                self.user_operation.response_elem_name, user_id)}
+        )
 
-    ###############################################################
-    @router.post("/users/create", response_model=UserResponsePostSchema, status_code=status.HTTP_201_CREATED)
-    def add_user(self, user: UserPostSchema) -> JSONResponse:
+    @router.patch("/users/{user_id}", **asdict(UserOutputPatch()))
+    def patch_user(self,
+                   user: UserInterfacePatch = Depends(UserInterfacePatch)
+                   ) -> JSONResponse:
+        """
+        Updates user data.
+        Only available to admins.
+        """
+        self.user_operation.update_obj(user.user_id, user.data)
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"message": get_text('patch').format(
+                self.user_operation.response_elem_name, user.user_id)}
+        )
+
+    @router.post("/users/create", **asdict(UserOutputPost()))
+    def add_user(self, user: UserInterfacePost = Depends(UserInterfacePost)) -> JSONResponse:
         """
         Adds new user into db.
         Only available to admins.
         """
-        user = self.user_operation.add_obj(user)
+        user = self.user_operation.add_obj(user.data)
 
-        return JSONResponse(status_code=status.HTTP_201_CREATED,
-                            content={"message": get_text('post').format(
-                                self.user_operation.response_elem_name, user.id)}
-                            )
+        return JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content={"message": get_text('post').format(
+                self.user_operation.response_elem_name, user.id)}
+        )
---
+++ src/swagger/user.py
@@ -0,0 +1,100 @@
+from dataclasses import dataclass
+from typing import Literal, Optional, Type, Any
+
+from fastapi import Query, Path, Body, status
+
+from src.schemes.user.base_schemes import (UserGetSchema,
+                                           UserPatchSchema,
+                                           UserPostSchema)
+from src.schemes.user.response_schemes import (UserResponsePatchSchema,
+                                               UserResponseDeleteSchema,
+                                               UserResponsePostSchema)
+
+
+@dataclass
+class UserInterfaceGetAll:
+    phone: str = Query(default=None, description="Phone number")
+    status: Literal['confirmed'] | Literal['unconfirmed'] = Query(
+        default=None, description="'confirmed' or 'unconfirmed'"
+    )
+    
+    
+@dataclass
+class UserInterfacePatch:
+    user_id: int = Path(..., ge=1)
+    data: UserPatchSchema = Body(..., example={
+        "username": "some_username",
+        "email": "user@example.com",
+        "phone": "123456789",
+        "role": "client",
+        "status": "unconfirmed"
+    })
+
+
+@dataclass
+class UserInterfacePost:
+    data: UserPostSchema = Body(..., example={
+        "username": "some_username",
+        "email": "user@example.com",
+        "phone": "123456789",
+        "role": "client",
+        "password": "some_strong_password"
+    })
+
+
+@dataclass
+class UserOutputGetAll:
+    summary: Optional[str] = 'Get all users by parameters'
+    description: Optional[str] = (
+        "**Returns** all users from db by **parameters**. <br />"
+        "Only available to **superuser.**"
+    )
+    response_model: Optional[Type[Any]] = list[UserGetSchema]
+    status_code: Optional[int] = status.HTTP_200_OK
+    response_description: str = 'List of users'
+    
+
+@dataclass
+class UserOutputGet:
+    summary: Optional[str] = 'Get user by user id'
+    description: Optional[str] = (
+        "**Returns** user from db by **user id**. <br />"
+        "Only available to **superuser.**"
+    )
+    response_model: Optional[Type[Any]] = UserGetSchema
+    status_code: Optional[int] = status.HTTP_200_OK
+    response_description: str = 'User data'
+    
+
+@dataclass
+class UserOutputDelete:
+    summary: Optional[str] = 'Delete user by user id'
+    description: Optional[str] = (
+        "**Deletes** user from db by **user id**. <br />"
+        "Only available to **superuser.**"
+    )
+    response_model: Optional[Type[Any]] = UserResponseDeleteSchema
+    status_code: Optional[int] = status.HTTP_200_OK
+    
+
+@dataclass
+class UserOutputPatch:
+    summary: Optional[str] = 'Patch user by user id'
+    description: Optional[str] = (
+        "**Updates** user from db by **user id**. <br />"
+        "Only available to **superuser.**"
+    )
+    response_model: Optional[Type[Any]] = UserResponsePatchSchema
+    status_code: Optional[int] = status.HTTP_200_OK
+
+
+@dataclass
+class UserOutputPost:
+    summary: Optional[str] = 'Post user by user id'
+    description: Optional[str] = (
+        "**Adds** new user into db. <br />"
+        "Only available to **superuser.**"
+    )
+    response_model: Optional[Type[Any]] = UserResponsePostSchema
+    status_code: Optional[int] = status.HTTP_201_CREATED
+
