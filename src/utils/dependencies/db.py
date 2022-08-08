fea62fd1b4874c204018623f7235b541b0fbc834
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-08-08 17:03:08 +0300

    Delete extra comments

--- src/utils/dependencies/db.py
+++ src/utils/dependencies/db.py
@@ -6,11 +6,6 @@ from src.db.db_sqlalchemy import SessionLocal
 def get_db() -> Generator:
     db = SessionLocal()
     try:
-        # print('Opening db connection...')
         yield db
-        # db.commit()
-    # except Exception:
-    #     db.rollback()
     finally:
-        # print('Closing db connection...')
         db.close()
