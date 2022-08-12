76bf566363c846615fd641611d1ed037f12c9de8
vandersvanlin <vandersvanlin@gmail.com>
2022-08-12 18:54:24 +0300

    Decompose docker-compose files

--- start.sh
+++ start.sh
@@ -2,8 +2,21 @@
 
 set -e
 
-set -a; source $PWD/.env; set +a
+set -a
+source $PWD/.env
+set +a
 
-docker compose -f docker/docker-compose.yml build
-
-docker compose -f docker/docker-compose.yml up -d
+case "$1" in
+--dev)
+  echo "The development container is building ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.dev.yml build
+  echo "The development container is running ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.dev.yml up
+  ;;
+*)
+  echo "The production container is building ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.prod.yml build
+  echo "The production container is running ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.prod.yml up -d
+  ;;
+esac
--- stop.sh
+++ stop.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-docker compose -f docker/docker-compose.yml stop
+case "$1" in
+--dev)
+  echo "The development container is stopping ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.dev.yml stop
+  echo "The development container is removing ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.dev.yml down
+  ;;
+*)
+  echo "The production container is stopping ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.prod.yml stop
+  echo "The production container is removing ..."
+  docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.prod.yml down
+esac
