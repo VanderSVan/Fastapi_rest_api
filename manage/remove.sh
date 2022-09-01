07102290f5005eb421558fd33d5a07faef35b30f
vandersvanlin <vandersvanlin@gmail.com>
2022-09-01 21:35:41 +0300

    Update docker-compose.prod.yml and docker-compose.dev.yml

--- manage/remove.sh
+++ manage/remove.sh
@@ -12,14 +12,16 @@ case "$1" in
 --dev)
   export COMPOSE_PROJECT_NAME=restaurant_dev
   echo "The development containers data are removing ..."
-  docker rmi restaurant_dev_backend
+  docker rmi restaurant_dev_flower
   docker rmi restaurant_dev_celery_worker
+  docker rmi restaurant_dev_backend
   docker volume rm restaurant_dev_restaurant-db
-  docker volume prune
   ;;
 *)
   export COMPOSE_PROJECT_NAME=restaurant
   echo "The production containers data are removing ..."
+  docker rmi restaurant_flower
+  docker rmi restaurant_celery_worker
   docker rmi restaurant_backend
   docker volume rm restaurant_restaurant-backend restaurant_restaurant-db
   ;;
